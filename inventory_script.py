dbe7831a8fdc4d86bed82ec729c36537c48211de
monty-morris <montymorris1@icloud.com>
2024-04-14 12:34:28 +0100

    new

--- inventory_script.py
+++ inventory_script.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 from datetime import datetime
+import gspread
+from google.oauth2.service_account import Credentials
 
 def get_next_tracking_number():
     if not os.path.exists("tracking_number.txt"):
@@ -36,22 +38,32 @@ def create_html_page(tracking_number, item_name):
     with open(f"{tracking_number}.html", "w") as file:
         file.write(html_content)
 
+def update_google_sheets(item_name, tracking_number):
+    scope = ['https://www.googleapis.com/auth/spreadsheets']
+    credentials = Credentials.from_service_account_file("/Users/monty/Documents/Inventory/credentials.json", scopes=scope)
+    client = gspread.authorize(credentials)
+    spreadsheet_id = "1M0XRvO3zvHtmNkB6NUFkW10bs2EWSUbuFmRrhZDotJY"
+    sheet = client.open_by_key(spreadsheet_id).sheet1
+    row = sheet.find(tracking_number).row
+    sheet.update_cell(row, 1, item_name)
+
 def main():
-    action = input("Enter item name or 'counter_reset' to reset tracking number: ").strip()
+    action = input("Enter item name: ").strip()
 
     if action == "counter_reset":
         subprocess.run(["git", "pull", "origin", "main"])
         subprocess.run(["rm", "tracking_number.txt"])
         subprocess.run(["touch", "tracking_number.txt"])
-        subprocess.run(["echo", "0000", ">", "tracking_number.txt"])
+        subprocess.run(["echo", "0001", ">", "tracking_number.txt"])
         subprocess.run(["git", "add", "."])
-        subprocess.run(["git", "commit", "-m", "'Reset tracking number to 0000'"])
+        subprocess.run(["git", "commit", "-m", "'Reset tracking number to 0001'"])
         subprocess.run(["git", "push", "origin", "main"])
-        print("Tracking number reset to 0000.")
+        print("Tracking number reset to 0001.")
     else:
         item_name = action
         tracking_number = get_next_tracking_number()
         create_html_page(tracking_number, item_name)
+        update_google_sheets(item_name, tracking_number)
         subprocess.run(["git", "pull", "origin", "main"])
         subprocess.run(["git", "add", "."])
         subprocess.run(["git", "commit", "-m", f"'Add item {tracking_number}: {item_name}'"])
